46a1ca95bd32a1769df6258ea163ddb553fe9247
Tony W <antonywiegand2006@gmail.com>
2026-02-28 16:23:45 -0500

    Added better comments to database files

--- Hub/db.py
+++ Hub/db.py
@@ -1,16 +1,26 @@
-"""
+""" Database Creation Function
+
+Reusable function for creating databases
 
 Antony Wiegand, McMaster, 2026"""
 
 from sqlmodel import SQLModel, create_engine
 
+# REQURED DO NOT DELETE
 from . import models
 
+#name and location of database
 sqlite_file_name = "hub/database.db"
 sqlite_url = f"sqlite:///{sqlite_file_name}"
 
+# creates and connects database to location
 connect_args = {"check_same_thread": False}
 engine = create_engine(sqlite_url, echo=True, connect_args=connect_args) # remove echo when in production
 
-def create_db_and_table():                  # lets us reuse creating a db (we need multiple dbs)
+def create_db_and_table():
+    """
+    Input: None
+    1. Creates Database \n
+    Output: None
+    """
     SQLModel.metadata.create_all(engine)
